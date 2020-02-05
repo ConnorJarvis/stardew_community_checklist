b7d1fa27156ed45ae66a7a26cb8735a6c8f64731
John Cleaver <cleaver.john.k@gmail.com>
2020-02-04 22:49:19 -0500

    Replace default components

---
+++ components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-navbar
+      class="shadow"
+      sticky="true"
+      fixed="top"
+      toggleable="lg"
+      type="dark"
+      variant="primary"
+    >
+      <b-navbar-brand href="#">
+        Stardew Community Checklist
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse" />
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">
+            Welcome
+          </b-nav-item>
+          <b-nav-item-dropdown text="Bundles" right />
+          <b-nav-item-dropdown text="Search" right />
+          <b-nav-form>
+            <b-button variant="outline-light">
+              <cog-outline />
+            </b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import CogOutline from 'mdi-vue/Cogs'
+export default {
+  components: {
+    CogOutline
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,24 +6,8 @@
         Stardew Community Checklist
       </h1>
       <h2 class="subtitle">
-        A checklist for Stardew Valley
+        Track your progress on the Community Center!
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
