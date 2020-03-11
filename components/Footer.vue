cac4da02380dd9b8b29046096c6c48bab49fd9a9
John Cleaver <cleaver.john.k@gmail.com>
2020-03-10 22:51:20 -0400

    Add footer

---
+++ components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer class="footer">
+    <div>
+      <div class="text-center bg-light">
+        <p>
+          <strong>Stardew Community Checklist</strong> by <a href="http://johncleaver.com">John Cleaver</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        </p>
+        <p>
+          <nuxt-link to="/changelog">
+            Changelog
+          </nuxt-link>
+        </p>
+        <p>
+          Bundle and Item information is from the <a href="http://stardewvalleywiki.com/Stardew_Valley_Wiki">Stardew Valley Wiki</a>
+          and is used under the <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY-NC-SA 3.0 License</a>.
+        </p>
+        <p>
+          <a href="http://stardewvalley.net/">Stardew Valley</a> &copy; <a href="https://chucklefish.org/">Chucklefish LTD</a>.
+          Developed by <a href="https://twitter.com/ConcernedApe">ConcernedApe</a>.
+        </p>
+        <p>
+          <a :href="gh_address"><font-awesome-icon :icon="gh_icon" /></a>
+          <a :href="reddit_address"><font-awesome-icon :icon="reddit_icon" /></a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+  data () {
+    return {
+      gh_address: 'https://github.com/kihashi/stardew_community_checklist',
+      reddit_address: 'https://www.reddit.com/r/stardewchecklist/'
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
