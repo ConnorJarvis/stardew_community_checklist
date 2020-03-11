d9d7ec9f3602cf507899b20f376f3183ed1e181f
John Cleaver <cleaver.john.k@gmail.com>
2020-03-10 22:50:27 -0400

    Update layout spacing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <navbar />
-    <nuxt />
+    <navbar class="mb-3" />
+    <b-container>
+      <b-row>
+        <b-col>
+          <nuxt class="min-vh-100" />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <app-footer class="mt-3" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
+import AppFooter from '~/components/Footer.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    AppFooter
   }
 }
 </script>
