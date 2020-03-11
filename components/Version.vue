5ab2c502959faabf84090e32a980795832794481
John Cleaver <cleaver.john.k@gmail.com>
2020-03-10 22:51:35 -0400

    Add changelog

---
+++ components/Version.vue
@@ -0,0 +1,61 @@
+<template>
+  <b-card>
+    <template v-slot:header>
+      <h4>{{ version.id }}</h4>
+    </template>
+
+    <ul>
+      <li v-for="change in version.changes" :key="change.date">
+        {{ change }}
+      </li>
+    </ul>
+
+    <template v-slot:footer>
+      <b-row class="text-center" align-v="center">
+        <b-col>
+          Release Date: {{ version.date }}
+        </b-col>
+        <b-col>
+          <b-button :href="release_url">
+            <github-box/> Release
+          </b-button>
+        </b-col>
+      </b-row>
+    </template>
+  </b-card>
+</template>
+
+<script>
+import GithubBox from 'mdi-vue/GithubBox'
+export default {
+  name: 'Version',
+  components: {
+    GithubBox
+  },
+  props: {
+    version: {
+      type: Object,
+      default () {
+        return {
+          id: 'Version 1.0',
+          date: '2016-04-13',
+          link: 'Version-1.0',
+          changes: [
+            'Initial Version'
+          ]
+        }
+      }
+    }
+  },
+  computed: {
+    release_url () {
+      return 'https://github.com/kihashi/stardew_community_checklist/releases/tag/' + this.version.link
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/changelog.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1 class="title">
+          Changelog
+        </h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <version v-for="version in changelog.versions" :key="version.date" :version="version" class="my-2" />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Version from '~/components/Version.vue'
+import ChangeLogJson from '~/assets/changelog.json'
+export default {
+  components: {
+    Version
+  },
+  data () {
+    return {
+      changelog: ChangeLogJson
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
