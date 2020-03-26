8fd1ee04c3a853375d8a0c4efc65492b12bb9f30
John Cleaver <cleaver.john.k@gmail.com>
2020-03-25 21:51:31 -0400

    Update store to work with Nuxt
    NOTE: This is using the deprecated classic mode. It will need to be changed before upgrading to Nuxt 3

--- store/index.js
+++ store/index.js
@@ -8,8 +8,6 @@ import RoomJson from '@/assets/game_data/rooms.json'
 import BundleJson from '@/assets/game_data/bundles.json'
 import ItemJson from '@/assets/game_data/items.json'
 
-Vue.use(Vuex)
-
 const prestate = {
   seasons: loadSeasons(),
   skills: loadSkills(),
@@ -19,135 +17,139 @@ const prestate = {
 prestate.bundles = loadBundles(prestate.rooms)
 prestate.items = loadItems(prestate.bundles, prestate.skills, prestate.seasons)
 
-export default new Vuex.Store({
-  state: {
-    StoredItems: {},
-    HideSpoilers: false,
-    HideCompleted: false,
-    BundleRewardsSpoilers: true,
-    ItemInfoSpoilers: true,
-    SeasonsSpoilers: true,
-    SkillsSpoilers: true,
-    CompactView: false
-  },
-  plugins: [
-    createPersistedState({
-      reducer: state => (
-        {
-          StoredItems: state.StoredItems,
-          HideSpoilers: state.HideSpoilers,
-          HideCompleted: state.HideCompleted,
-          BundleRewardsSpoilers: state.BundleRewardsSpoilers,
-          ItemInfoSpoilers: state.ItemInfoSpoilers,
-          SeasonsSpoilers: state.SeasonsSpoilers,
-          SkillsSpoilers: state.SkillsSpoilers,
-          CompactView: state.CompactView
-        }
-      )
-    })
-  ],
-  getters: {
-    IsBundleItemRedeemed: state => (BundleItem) => {
-      return state.StoredItems.hasOwnProperty(BundleItem.id)
+const createState = () => {
+  return new Vuex.Store({
+    state: {
+      StoredItems: {},
+      HideSpoilers: false,
+      HideCompleted: false,
+      BundleRewardsSpoilers: true,
+      ItemInfoSpoilers: true,
+      SeasonsSpoilers: true,
+      SkillsSpoilers: true,
+      CompactView: false
     },
-    GetBundleById: state => (bundleId) => {
-      return getById(state.bundles, bundleId)
-    },
-    GetItemById: state => (itemId) => {
-      return getById(state.items, itemId)
-    },
-    GetSerializedState: (state) => {
-      return btoa(JSON.stringify(state.StoredItems))
-    },
-    GetBundleItemsRedeemed: (state, getters) => (bundle) => {
-      return Math.min(bundle.items.reduce((redeemded, item) => {
-        if (getters.IsBundleItemRedeemed(item)) { redeemded++ }
-        return redeemded
-      }, 0), bundle.items_required)
-    },
-    IsBundleComplete: (state, getters) => (bundle) => {
-      return getters.GetBundleItemsRedeemed(bundle) >= bundle.items_required
-    },
-    GetRoomItemsRedeemed: (state, getters) => (room) => {
-      return room.bundles.reduce((redeemed, bundle) => {
-        return redeemed + getters.GetBundleItemsRedeemed(bundle)
-      }, 0)
-    },
-    IsRoomComplete: (state, getters) => (room) => {
-      return getters.GetRoomItemsRedeemed(room) >= room.items_required
-    },
-    GetOpenBundleItems: (state, getters) => (bundleId, itemId) => {
-      return (getters.GetBundleById(bundleId)).items.filter(bundleItem => bundleItem.item.id === itemId && !getters.IsBundleItemRedeemed(bundleItem))
-    }
-  },
-  mutations: {
-    SetSerializedState (state, SerializedState) {
-      state.StoredItems = JSON.parse(atob(SerializedState))
-    },
-    RedeemItem (state, BundleItem) {
-      Vue.set(state.StoredItems, BundleItem.id, 1)
-    },
-    UndoRedeemItem (state, BundleItem) {
-      Vue.delete(state.StoredItems, BundleItem.id)
-    },
-    toggleSpoilers (state) {
-      state.HideSpoilers = !state.HideSpoilers
-    },
-    toggleCompleted (state) {
-      state.HideCompleted = !state.HideCompleted
-    },
-    toggleBundleRewards (state) {
-      state.BundleRewardsSpoilers = !state.BundleRewardsSpoilers
-    },
-    toggleItemInfo (state) {
-      state.ItemInfoSpoilers = !state.ItemInfoSpoilers
-    },
-    toggleSeasons (state) {
-      state.SeasonsSpoilers = !state.SeasonsSpoilers
-    },
-    toggleSkills (state) {
-      state.SkillsSpoilers = !state.SkillsSpoilers
-    },
-    toggleCompactView (state) {
-      state.CompactView = !state.CompactView
-    },
-    initState (state) {
-      Vue.set(state, 'seasons', prestate.seasons)
-      Vue.set(state, 'skills', prestate.skills)
-      Vue.set(state, 'rooms', prestate.rooms)
-      Vue.set(state, 'bundles', prestate.bundles)
-      Vue.set(state, 'items', prestate.items)
-    },
-    resetData (state) {
-      state.StoredItems = {}
-    }
-  },
-  actions: {
-    LoadV1State (context, v1state) {
-      for (let i = 0; i < v1state.length; i++) {
-        for (let j = 0; j < v1state[i].length; j++) {
-          let itemId = v1state[i][j].item
-          // Patch for duplicate oak
-          if (itemId === 116) {
-            itemId = 24
+    plugins: [
+      createPersistedState({
+        reducer: state => (
+          {
+            StoredItems: state.StoredItems,
+            HideSpoilers: state.HideSpoilers,
+            HideCompleted: state.HideCompleted,
+            BundleRewardsSpoilers: state.BundleRewardsSpoilers,
+            ItemInfoSpoilers: state.ItemInfoSpoilers,
+            SeasonsSpoilers: state.SeasonsSpoilers,
+            SkillsSpoilers: state.SkillsSpoilers,
+            CompactView: state.CompactView
           }
+        )
+      })
+    ],
+    getters: {
+      IsBundleItemRedeemed: state => (BundleItem) => {
+        return state.StoredItems.hasOwnProperty(BundleItem.id)
+      },
+      GetBundleById: state => (bundleId) => {
+        return getById(state.bundles, bundleId)
+      },
+      GetItemById: state => (itemId) => {
+        return getById(state.items, itemId)
+      },
+      GetSerializedState: (state) => {
+        return btoa(JSON.stringify(state.StoredItems))
+      },
+      GetBundleItemsRedeemed: (state, getters) => (bundle) => {
+        return Math.min(bundle.items.reduce((redeemded, item) => {
+          if (getters.IsBundleItemRedeemed(item)) { redeemded++ }
+          return redeemded
+        }, 0), bundle.items_required)
+      },
+      IsBundleComplete: (state, getters) => (bundle) => {
+        return getters.GetBundleItemsRedeemed(bundle) >= bundle.items_required
+      },
+      GetRoomItemsRedeemed: (state, getters) => (room) => {
+        return room.bundles.reduce((redeemed, bundle) => {
+          return redeemed + getters.GetBundleItemsRedeemed(bundle)
+        }, 0)
+      },
+      IsRoomComplete: (state, getters) => (room) => {
+        return getters.GetRoomItemsRedeemed(room) >= room.items_required
+      },
+      GetOpenBundleItems: (state, getters) => (bundleId, itemId) => {
+        return (getters.GetBundleById(bundleId)).items.filter(bundleItem => bundleItem.item.id === itemId && !getters.IsBundleItemRedeemed(bundleItem))
+      }
+    },
+    mutations: {
+      SetSerializedState (state, SerializedState) {
+        state.StoredItems = JSON.parse(atob(SerializedState))
+      },
+      RedeemItem (state, BundleItem) {
+        Vue.set(state.StoredItems, BundleItem.id, 1)
+      },
+      UndoRedeemItem (state, BundleItem) {
+        Vue.delete(state.StoredItems, BundleItem.id)
+      },
+      toggleSpoilers (state) {
+        state.HideSpoilers = !state.HideSpoilers
+      },
+      toggleCompleted (state) {
+        state.HideCompleted = !state.HideCompleted
+      },
+      toggleBundleRewards (state) {
+        state.BundleRewardsSpoilers = !state.BundleRewardsSpoilers
+      },
+      toggleItemInfo (state) {
+        state.ItemInfoSpoilers = !state.ItemInfoSpoilers
+      },
+      toggleSeasons (state) {
+        state.SeasonsSpoilers = !state.SeasonsSpoilers
+      },
+      toggleSkills (state) {
+        state.SkillsSpoilers = !state.SkillsSpoilers
+      },
+      toggleCompactView (state) {
+        state.CompactView = !state.CompactView
+      },
+      initState (state) {
+        Vue.set(state, 'seasons', prestate.seasons)
+        Vue.set(state, 'skills', prestate.skills)
+        Vue.set(state, 'rooms', prestate.rooms)
+        Vue.set(state, 'bundles', prestate.bundles)
+        Vue.set(state, 'items', prestate.items)
+      },
+      resetData (state) {
+        state.StoredItems = {}
+      }
+    },
+    actions: {
+      LoadV1State (context, v1state) {
+        for (let i = 0; i < v1state.length; i++) {
+          for (let j = 0; j < v1state[i].length; j++) {
+            let itemId = v1state[i][j].item
+            // Patch for duplicate oak
+            if (itemId === 116) {
+              itemId = 24
+            }
 
-          // Patch for parsnip
-          if (itemId === 26 && i === 6) {
-            itemId = 27
-          }
-          const bundleItems = context.getters.GetOpenBundleItems(i, itemId)
-          if (bundleItems.length > 0) {
-            context.commit('RedeemItem', bundleItems[0])
-          } else {
+            // Patch for parsnip
+            if (itemId === 26 && i === 6) {
+              itemId = 27
+            }
+            const bundleItems = context.getters.GetOpenBundleItems(i, itemId)
+            if (bundleItems.length > 0) {
+              context.commit('RedeemItem', bundleItems[0])
+            } else {
             // eslint-disable-next-line
             console.log(`Tried to redeem bundle ${i} item ${itemId}, but no open bundle items were found`)
+            }
           }
         }
       }
     }
-  }
-})
+  })
+}
+
+export default createState
 
 function getById (array, id) {
   // eslint-disable-next-line
