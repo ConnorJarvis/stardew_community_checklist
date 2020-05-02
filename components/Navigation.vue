2e695bc77a27d027935251a865fc1259f1f0c0a9
John Cleaver <cleaver.john.k@gmail.com>
2020-05-02 16:28:21 -0400

    Add navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-navbar type="is-info" shadow wrapper-class="container">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <h1 class="is-size-3 has-text-weight-semibold is-hidden-mobile">
+          Stardew Community Checklist
+        </h1>
+        <h1 class="is-size-3 has-text-weight-semibold is-hidden-tablet">
+          SCC
+        </h1>
+      </b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="nuxt-link" to="/">
+        Welcome
+      </b-navbar-item>
+      <b-navbar-item>
+        Bundles
+      </b-navbar-item>
+      <b-navbar-item>
+        Search
+      </b-navbar-item>
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <b-button type="is-light" icon-left="cogs" outlined="true" />
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+
+}
+</script>
+
+<style>
+
+</style>
