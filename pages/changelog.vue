0bc206c5abe5d3708879b81eb16658d78ae5f5a3
John Cleaver <cleaver.john.k@gmail.com>
2020-05-02 23:55:02 -0400

    Add changelog

---
+++ pages/changelog.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <section class="container">
+      <h1 class="title">
+        Changelog
+      </h1>
+      <version v-for="version in changelog.versions" :key="version.id" :version="version" />
+    </section>
+  </section>
+</template>
+
+<script>
+import ChangeLogJson from '@/assets/game_data/changelog.json'
+import Version from '@/components/Version.vue'
+export default {
+  name: 'Changelog',
+  components: {
+    Version
+  },
+  data () {
+    return {
+      changelog: ChangeLogJson
+    }
+  }
+}
+</script>
+
+<style>
+</style>
